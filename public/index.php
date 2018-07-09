740e7c0b76b69979a815ed25123f5b60b5ec1cfd
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2018-07-09 13:46:19 +0200

    Index and header pages

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "partials/header.php" ?>
+<body>
+    <div class="container">
+        <h1>List of Tasks</h1>
+        <div class="table">
+            <table class="table table-responsive table-striped table-hover">
+                <thead>
+                    <th>Title</th>
+                    <th>Process at</th>
+                    <th>Complete at</th>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
