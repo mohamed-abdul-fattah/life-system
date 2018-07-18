2566af129f48fdeff7ae15e24292dd941f398ce8
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2018-07-19 00:37:53 +0200

    Create expense form

--- public/expenses/create.php
+++ public/expenses/create.php
@@ -18,15 +18,46 @@
                             <i class="fa fa-arrow-left" aria-hidden="true"></i>
                             Back
                         </a>
+                        <hr>
                     </div>
-                    <div class="panel-body">
-                        <div class="form-group">
-                            <label for="amount" class="col-form-label">Amount</label>
-                            <input type="number" class="form-control">
-                            <div class="col-md-6">
+                    <form action="<?php echo url('expenses') ?>" method="POST">
+                        <!-- Amount -->
+                        <div class="panel-body">
+                            <div class="form-group row">
+                                <label for="amount" class="col-sm-2 col-form-label">Amount</label>
+                                <div class="col-sm-10">
+                                    <input id="amount" name="amount" type="number" class="form-control" required>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                        <!-- Comment -->
+                        <div class="panel-body">
+                            <div class="form-group row">
+                                <label for="comment" class="col-sm-2 col-form-label">Comment</label>
+                                <div class="col-sm-10">
+                                    <textarea name="Comment" id="comment" cols="30" rows="5" class="form-control" required></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Date -->
+                        <div class="panel-body">
+                            <div class="form-group row">
+                                <label for="created_at" class="col-sm-2 col-form-label">Date</label>
+                                <div class="col-sm-10">
+                                    <input id="created_at" name="created_at" type="date" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <!-- Submit button -->
+                        <div class="panel-body">
+                            <div class="form-group row">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
