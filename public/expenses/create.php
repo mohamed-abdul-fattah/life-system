b713801825d716ad33ea8da43ba1d0dcd1663d08
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2018-07-19 00:26:06 +0200

    Updated bootstrap grid system

--- public/expenses/create.php
+++ public/expenses/create.php
@@ -10,16 +10,23 @@
     <?php include public_path("layouts/navbar.php") ?>
     <div class="container">
         <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h2 class="title">Add Expense</h2>
-                    <a href="<?php echo url('expenses') ?>" class="btn btn-warning btn-sm float-right">
-                        <i class="fa fa-arrow-left" aria-hidden="true"></i>
-                        Back
-                    </a>
-                </div>
-                <div class="panel-body">
-                    
+            <div class="col-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="title">Add Expense</h2>
+                        <a href="<?php echo url('expenses') ?>" class="btn btn-warning btn-sm float-right">
+                            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                            Back
+                        </a>
+                    </div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label for="amount" class="col-form-label">Amount</label>
+                            <input type="number" class="form-control">
+                            <div class="col-md-6">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- public/expenses/index.php
+++ public/expenses/index.php
@@ -26,64 +26,66 @@
     <?php include public_path("layouts/navbar.php") ?>
     <div class="container">
         <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h2 class="title">Expenses</h2>
-                    <a href="<?php echo url('expenses/create.php') ?>" class="btn btn-primary btn-sm float-right">
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                        Add Expense
-                    </a>
-                </div>
-                <div class="panel-body">
-                    <div class="table">
-                        <table class="table table-responsive table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th>Amount</th>
-                                <th>Comment</th>
-                                <th>Date</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <?php while ($expense = mysqli_fetch_object($expenses)): ?>
+            <div class="col-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2 class="title">Expenses</h2>
+                        <a href="<?php echo url('expenses/create.php') ?>" class="btn btn-primary btn-sm float-right">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                            Add Expense
+                        </a>
+                    </div>
+                    <div class="panel-body">
+                        <div class="table">
+                            <table class="table table-striped table-hover">
+                                <thead>
                                 <tr>
-                                    <td><?php echo $expense->amount ?></td>
-                                    <td><?php echo $expense->comment ?></td>
-                                    <td><?php echo date('d-M-Y', strtotime($expense->created_at)) ?></td>
+                                    <th>Amount</th>
+                                    <th>Comment</th>
+                                    <th>Date</th>
                                 </tr>
-                                <?php endwhile; ?>
-                            </tbody>
-                        </table>
-
-                        <!-- Pagination -->
-                        <?php if ($total): ?>
-                            <nav aria-label="...">
-                                <ul class="pagination">
-                                    <li class="page-item <?php if ($currentPage == $firstPage) echo 'disabled'; ?>">
-                                        <a class="page-link" href="/expenses.php?page=<?php echo $currentPage - 1 ?>">
-                                            Previous
-                                        </a>
-                                    </li>
-                                    <?php for ($page = 1; $page <= ceil($total / $perPage); $page++): ?>
-                                        <li class="page-item <?php if ($page == $currentPage) echo 'active'; ?>">
-                                            <?php if ($page == $currentPage): ?>
-                                                <span class="page-link">
-                                                    <?php echo $page ?>
-                                                    <span class="sr-only">(current)</span>
-                                                </span>
-                                            <?php else: ?>
-                                                <a class="page-link" href="/expenses.php?page=<?php echo $page ?>">
-                                                    <?php echo $page ?>
-                                                </a>
-                                            <?php endif; ?>
+                                </thead>
+                                <tbody>
+                                    <?php while ($expense = mysqli_fetch_object($expenses)): ?>
+                                    <tr>
+                                        <td><?php echo $expense->amount ?></td>
+                                        <td><?php echo $expense->comment ?></td>
+                                        <td><?php echo date('d-M-Y', strtotime($expense->created_at)) ?></td>
+                                    </tr>
+                                    <?php endwhile; ?>
+                                </tbody>
+                            </table>
+    
+                            <!-- Pagination -->
+                            <?php if ($total): ?>
+                                <nav aria-label="...">
+                                    <ul class="pagination">
+                                        <li class="page-item <?php if ($currentPage == $firstPage) echo 'disabled'; ?>">
+                                            <a class="page-link" href="/expenses.php?page=<?php echo $currentPage - 1 ?>">
+                                                Previous
+                                            </a>
                                         </li>
-                                    <?php endfor ?>
-                                    <li class="page-item <?php if ($currentPage == $lastPage) echo 'disabled'; ?>">
-                                        <a class="page-link" href="/expenses.php?page=<?php echo $currentPage + 1 ?>">Next</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        <?php endif ?>
+                                        <?php for ($page = 1; $page <= ceil($total / $perPage); $page++): ?>
+                                            <li class="page-item <?php if ($page == $currentPage) echo 'active'; ?>">
+                                                <?php if ($page == $currentPage): ?>
+                                                    <span class="page-link">
+                                                        <?php echo $page ?>
+                                                        <span class="sr-only">(current)</span>
+                                                    </span>
+                                                <?php else: ?>
+                                                    <a class="page-link" href="/expenses.php?page=<?php echo $page ?>">
+                                                        <?php echo $page ?>
+                                                    </a>
+                                                <?php endif; ?>
+                                            </li>
+                                        <?php endfor ?>
+                                        <li class="page-item <?php if ($currentPage == $lastPage) echo 'disabled'; ?>">
+                                            <a class="page-link" href="/expenses.php?page=<?php echo $currentPage + 1 ?>">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            <?php endif ?>
+                        </div>
                     </div>
                 </div>
             </div>
