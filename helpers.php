5fe8878cbbc69ef500b716445ccbe1e29f2cb9df
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2018-07-19 00:03:57 +0200

    url helper function to get the website url

--- helpers.php
+++ helpers.php
@@ -8,10 +8,40 @@ if (! function_exists('public_path'))
      * @param  string $path
      * @return string 
      */
-    function public_path(string $path = null): string
+    function public_path(string $path = ""): string
     {
-        $path = ($path[0] === '/') ? substr($path, 1) : $path;
+        $path = ($path) ? leading_slash($path) : null;
 
         return __DIR__. "/public/" . $path;
+    }
+}
+
+if (! function_exists('leading_slash'))
+{
+    /**
+     * Make sure that the given string has no leading back-slash
+     *
+     * @param  string $path
+     * @return string
+     */
+    function leading_slash(string $path): string
+    {
+        return ($path[0] === '/') ? substr($path, 1) : $path;
+    }
+}
+
+if (! function_exists('url'))
+{
+    /**
+     * Return the website URL
+     *
+     * @param  string $url
+     * @return string
+     */
+    function url(string $url = null): string
+    {
+        $url = ($url) ? leading_slash($url) : null;
+
+        return "http://life-system.local/". $url;
     }
 }
