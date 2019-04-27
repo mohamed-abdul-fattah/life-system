e886256564f6900b6f68da16a04cd561bcdfd4e6
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-04-27 17:11:35 +0200

    Configuration variables

---
+++ configs.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * System Configuration Variables.
+ */
+return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'database name',
+    'DB_USER' => 'database username',
+    'DB_PASS' => 'database password',
+];
