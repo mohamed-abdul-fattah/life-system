8cc8d9c1674ccf9bc986b146bd3fa5185edd0ae2
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-05-11 06:08:31 +0200

    Moved database functions to separate helper file

--- helpers.php
+++ helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/sessions.php';
+require_once __DIR__ . '/databases.php';
 
 if ( ! function_exists('public_path') ) {
     /**
@@ -128,62 +129,7 @@ if ( ! function_exists('getConfig') ) {
     }
 }
 
-if ( ! function_exists('open_connection') ) {
-    /**
-     * Open database connection
-     *
-     * @return false|mysqli
-     * @throws Exception
-     */
-    function open_connection()
-    {
-        $connection = mysqli_connect(
-            getConfig('DB_HOST'),
-            getConfig('DB_USER'),
-            getConfig('DB_PASS'),
-            getConfig('DB_NAME')
-        );
 
-        if ( mysqli_connect_errno() ) {
-            throw new Exception('Connection failed: ' . mysqli_connect_error(), 500);
-        }
-
-        set_charset($connection, 'utf8');
-
-        return $connection;
-    }
-}
-
-if ( ! function_exists('set_charset') ) {
-    /**
-     * Set database charset names.
-     *
-     * @param mysqli $connection
-     * @param string $charset
-     * @throws Exception
-     */
-    function set_charset($connection, $charset)
-    {
-        if ( ! mysqli_set_charset($connection, $charset) ) {
-            throw new Exception(
-                "Error loading character set {$charset} " . mysqli_error($connection),
-                500
-            );
-        }
-    }
-}
-
-if ( ! function_exists('close_connection') ) {
-    /**
-     * Close database connection
-     *
-     * @param mysqli $connection
-     */
-    function close_connection($connection)
-    {
-        mysqli_close($connection);
-    }
-}
 
 if ( ! function_exists('dd') ) {
     /**
