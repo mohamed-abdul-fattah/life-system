c2d01ca8262bb9f42c1defb448e6a4321b57d1d6
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-05-11 05:30:31 +0200

    Fixed unwanted spaces in edit comment textarea

--- public/expenses/edit.php
+++ public/expenses/edit.php
@@ -57,9 +57,7 @@ include public_path('layouts/header.php');
                                                   cols="30"
                                                   rows="5"
                                                   class="form-control"
-                                                  required="required">
-                                            <?php echo nl2br(htmlentities($expense->comment)) ?>
-                                        </textarea>
+                                                  required="required"><?php echo nl2br(htmlentities($expense->comment)) ?></textarea>
                                 </div>
                             </div>
                             <!-- Date -->
