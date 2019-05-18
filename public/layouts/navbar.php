1a7a6c4c0d7fdaaf9ea5f6d11da53e7acaaeb77f
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-05-18 14:14:30 +0200

    Updated Logo and Home URLs to use helper baseUrl

--- public/layouts/navbar.php
+++ public/layouts/navbar.php
@@ -1,13 +1,13 @@
 <div class="header">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">
+        <a class="navbar-brand" href="<?php echo url('/') ?>">
             <h2><?php echo getConfig('APP_NAME', 'Life System') ?></h2>
         </a>
         <div id="navbarNav">
             <ul class="navbar-nav">
                 <?php $activeItem = $activeItem ?? 'home'; ?>
                 <li class="nav-item <?php if ( $activeItem === 'home' ) echo "active"; ?>">
-                    <a class="nav-link" href="/">Home</a>
+                    <a class="nav-link" href="<?php echo url('/') ?>">Home</a>
                 </li>
                 <li class="nav-item <?php if ( $activeItem === 'expenses' ) echo "active"; ?>">
                     <a class="nav-link" href="<?php echo url('expenses') ?>">Expenses</a>
