41cd5640720a65333a5ada53337421aa75e06f82
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-06-29 16:47:48 +0200

    Unit test the pagination indexes helper method

--- tests/Unit/PaginationTest.php
+++ tests/Unit/PaginationTest.php
@@ -18,6 +18,14 @@ class PaginationTest extends TestCase
                 ]
             ],
             [
+                2,
+                2,
+                [
+                    0 => 1,
+                    1 => 2,
+                ]
+            ],
+            [
                 1,
                 20,
                 [
@@ -77,11 +85,215 @@ class PaginationTest extends TestCase
     }
 
     /**
+     * @param int $current
+     * @param int $last
+     * @param array $range
      * @test
      * @dataProvider rangesProvider
      */
-    public function testPaginationRange($current, $last, $range)
+    public function paginationRange($current, $last, $range)
     {
         $this->assertEquals(getPaginationRange($current, $last), $range);
+    }
+
+    public function paginationIndexesProvider()
+    {
+        return [
+            [
+                1,
+                2,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=0'),
+                        'disability' => true,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=2'),
+                        'disability' => false,
+                    ],
+                    'indexes'   => [
+                        0 => 1,
+                        1 => 2,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => false,
+                        'active'  => true
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=2'),
+                        'display' => false,
+                        'active'  => false
+                    ]
+                ]
+            ],
+            [
+                2,
+                2,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=1'),
+                        'disability' => false,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=3'),
+                        'disability' => true,
+                    ],
+                    'indexes'   => [
+                        0 => 1,
+                        1 => 2,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => false,
+                        'active'  => false,
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=2'),
+                        'display' => false,
+                        'active'  => true,
+                    ]
+                ]
+            ],
+            [
+                1,
+                20,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=0'),
+                        'disability' => true,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=2'),
+                        'disability' => false,
+                    ],
+                    'indexes'   => [
+                        0 => 1,
+                        1 => 2,
+                        2 => 3,
+                        3 => 4,
+                        4 => 5,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => false,
+                        'active'  => true
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=20'),
+                        'display' => true,
+                        'active'  => false
+                    ]
+                ]
+            ],
+            [
+
+                10,
+                20,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=9'),
+                        'disability' => false,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=11'),
+                        'disability' => false,
+                    ],
+                    'indexes'   => [
+                        0 => 8,
+                        1 => 9,
+                        2 => 10,
+                        3 => 11,
+                        4 => 12,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => true,
+                        'active'  => false
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=20'),
+                        'display' => true,
+                        'active'  => false
+                    ]
+                ]
+            ],
+            [
+
+                19,
+                20,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=18'),
+                        'disability' => false,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=20'),
+                        'disability' => false,
+                    ],
+                    'indexes'   => [
+                        0 => 16,
+                        1 => 17,
+                        2 => 18,
+                        3 => 19,
+                        4 => 20,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => true,
+                        'active'  => false
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=20'),
+                        'display' => false,
+                        'active'  => false
+                    ]
+                ]
+            ],
+            [
+                20,
+                20,
+                [
+                    'prev'      => [
+                        'URL'        => url('/expenses/?page=19'),
+                        'disability' => false,
+                    ],
+                    'next'      => [
+                        'URL'        => url('/expenses/?page=21'),
+                        'disability' => true,
+                    ],
+                    'indexes'   => [
+                        0 => 16,
+                        1 => 17,
+                        2 => 18,
+                        3 => 19,
+                        4 => 20,
+                    ],
+                    'firstPage' => [
+                        'URL'     => url('/expenses/'),
+                        'display' => true,
+                        'active'  => false
+                    ],
+                    'lastPage'  => [
+                        'URL'     => url('/expenses/?page=20'),
+                        'display' => false,
+                        'active'  => true
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @param int $current
+     * @param int $last
+     * @param array $paginationInfo
+     * @test
+     * @throws \Exception
+     * @dataProvider paginationIndexesProvider
+     */
+    public function paginationInfo($current, $last, $paginationInfo)
+    {
+        $this->assertEquals(paginationIndexes($current, $last), $paginationInfo);
     }
 }
