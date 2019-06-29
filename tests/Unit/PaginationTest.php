ec7f50fd4c202a6ab88d2e4690bd189b9c14018d
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-06-29 13:29:32 +0200

    Prepared for Pagination unit testing

---
+++ tests/Unit/PaginationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class PaginationTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testPaginationRange()
+    {
+        //
+    }
+}
