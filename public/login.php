d2dfea98ac44779a817913e019b86e29674ef9c2
alamir <enginerralamiir@gmail.com>
2020-11-03 16:15:27 +0200

    [ issue : 7 ] modified comments

--- public/login.php
+++ public/login.php
@@ -24,17 +24,6 @@ guest_check();
                 unset($_SESSION['errors']);
                 endif;
                 ?>
-        <!-- <div class='both'> -->
-<!-- sign up  -->
-        <!-- <div class="signup">
-        <div class="signup-connect">
-            
-            <a href="#" class="social-btn btn-social btn-facebook"><i class="fa fa-facebook"></i> Sign in with Facebook</a>
-            <a href="#" class="social-btn btn-social btn-twitter"><i class="fa fa-twitter"></i> Sign in with Twitter</a>
-            <a href="#" class="social-btn btn-social btn-google"><i class="fa fa-google"></i> Sign in with Google</a>
-            <a href="#" class="social-btn btn-social btn-linkedin"><i class="fa fa-linkedin"></i> Sign in with Linkedin</a>
-        </div>
-        </div> -->
 <!-- sign in  -->
         <form class="sign-in-form" action="<?php echo url('/loginPost.php') ?>" method="POST">        
             <div class="segment">
@@ -48,12 +37,6 @@ guest_check();
                 <input type="password" id="password" name="password" placeholder="Password"/>
             </label>
             <a href="#"><button class="red"><i class="icon ion-md-lock"></i>Log in</button></a>
-            <!-- <div class="small-social">
-                <a href="#"><button> <i class="fa fa-facebook"> </button></i></a>
-                <a href="#"><button><i class="fa fa-twitter"></button></i></a>
-                <a href="#"><button><i class="fa fa-google"></button></i></a>
-                <a href="#"><button><i class="fa fa-linkedin"></button></i></a>
-            </div> -->
         </form>
     </div>
 </div>
