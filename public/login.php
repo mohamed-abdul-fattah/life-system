3ddc8b18836622f83c5de97b62a68037799c3a2b
El-amir <engineeralamiir@gmail.com>
2020-12-10 19:38:45 +0200

    Simple edits on home page

--- public/login.php
+++ public/login.php
@@ -7,50 +7,52 @@ guest_check();
 <?php $title = 'Login' ?>
 <?php include public_path('layouts/header.php'); ?>
 <body>
-<section class="login">
+<div class="login">
+
 <!-- navbar -->
-<div>
-    <?php include public_path("layouts/navbar.php") ?>
-</div>
+    <section>
+        <?php include public_path("layouts/navbar.php") ?>
+    </section>
 
 <!-- error message -->
-<div>
-    <?php if ( isset($_SESSION['errors']) ): ?>
-        <div class="alert alert-danger">
-            <ul>
-                <?php foreach ( $_SESSION['errors'] as $error ): ?>
-                    <li><?php echo $error ?></li>
-                <?php endforeach ?>
-            </ul>
-        </div>
-    <?php
-    unset($_SESSION['errors']);
-    endif;
-    ?>   
-</div>
+    <section>
+        <?php if ( isset($_SESSION['errors']) ): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php foreach ( $_SESSION['errors'] as $error ): ?>
+                        <li><?php echo $error ?></li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        <?php
+        unset($_SESSION['errors']);
+        endif;
+        ?>   
+    </section>
 
 <!-- sign in  -->
-<div class="form">
-    <form class="form__signin" action="<?php echo url('/loginPost.php') ?>" method="POST">        
-        <h1 class="form__signin--header header-1">
-            sign in 
-        </h1>
+    <section class="form">
+        <form class="form__signin" action="<?php echo url('/loginPost.php') ?>" method="POST">        
+            <h1 class="form__signin--header header-1">
+                sign in 
+            </h1>
+            
+            <label for='email' class="form__signin--eamil" >
+                <input type="email" id="email" name="email" class="form__input" placeholder="Email Address"/>
+            </label>
+            <label for='password' class="form__signin--password">
+                <input type="password" id="password" name="password" class="form__input" placeholder="Password"/>
+            </label>
+            <a href="#"><button class="btn btn-white form__signin--btn"><i class="icon icon-md-lock"></i>Log in</button></a>
+        </form>
+    </section>
         
-        <label for='email' class="form__signin--eamil" >
-            <input type="email" id="email" name="email" class="form__input" placeholder="Email Address"/>
-        </label>
-        <label for='password' class="form__signin--password">
-            <input type="password" id="password" name="password" class="form__input" placeholder="Password"/>
-        </label>
-        <a href="#"><button class="btn btn-white form__signin--btn"><i class="icon icon-md-lock"></i>Log in</button></a>
-    </form>
-</div>
-    
 <!-- footer -->
-<div class="footer">
-    <?php include public_path("layouts/footer.php") ?>
+    <section class="footer">
+        <?php include public_path("layouts/footer.php") ?>
+    </section>
+
 </div>
-</section>
 
 </body>
 </html>
