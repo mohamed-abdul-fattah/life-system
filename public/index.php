d59492387feecbd7b0dce81f4b05c3a7cc6e277a
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-12-28 16:40:24 +0200

    [Issue-63] Remove all unneeded design to start over

--- public/index.php
+++ public/index.php
@@ -12,14 +12,6 @@ $query = "SELECT SUM(`amount`) AS 'total' FROM `transactions` WHERE `created_at`
 $sumQuery = mysqli_query($connection, $query);
 $monthTotal = mysqli_fetch_object($sumQuery)->total;
 ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js" 
-        integrity="sha512-SuxO9djzjML6b9w9/I07IWnLnQhgyYVSpHZx0JV97kGBfTIsUYlWflyuW4ypnvhBrslz1yJ3R+S14fdCWmSmSA==" 
-        crossorigin="anonymous">
-</script>
-
-<script defer src="<?php echo url('assets/js/chart.js') ?>"></script>
-<script defer src="<?php echo url('assets/js/globalFunctions.js') ?>"></script>
-
 <body>
     <div class="home">
         <section>
@@ -32,19 +24,6 @@ $monthTotal = mysqli_fetch_object($sumQuery)->total;
         </section>
         <section class="home__expenses">
             <div class="card">
-                <div class="card__chart">
-                    <div class="card__chart--container">
-                        <canvas id="chart" ></canvas>
-                    </div>
-                </div>
-                <div class="card__btns">
-                    <button class="btn card__btns--display" ><img class="card__btns--icon" src="https://img.icons8.com/fluent/48/000000/expand-arrow.png"/></button>
-                    <button class="btn card__btns--line"    ><img class="card__btns--icon" src="https://img.icons8.com/officel/80/000000/area-chart.png"/></button>
-                    <button class="btn card__btns--bar"     ><img class="card__btns--icon" src="https://img.icons8.com/offices/30/000000/bar-chart.png"/></button>
-                    <button class="btn card__btns--doughnut"><img class="card__btns--icon" src="https://img.icons8.com/officel/80/000000/doughnut-chart.png"/></button>
-                    <button class="btn card__btns--polar"   ><img class="card__btns--icon" src="https://img.icons8.com/ultraviolet/40/000000/rebalance-portfolio.png"/></button>
-                    <button class="btn card__btns--legends" ><img class="card__btns--icon" src="https://img.icons8.com/fluent/48/000000/arrow.png"/></button>
-                </div>
                 <div class="card__data">
                     <p class="month-summary">Money spent monthly</p>
                     <p class="month-summary
@@ -60,11 +39,9 @@ $monthTotal = mysqli_fetch_object($sumQuery)->total;
                     </p>                        
                 </div>
             </div>
-            <div class="legend"></div>
-                
         </section>
         <section class='footer'>
-                    <?php include public_path("layouts/footer.php") ?>
+            <?php include public_path("layouts/footer.php") ?>
         </section>
     </div>
 </body>
